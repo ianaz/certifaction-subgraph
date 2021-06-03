375601622cc099ff3288fd8e22fa358c31052d14
Silvio Rainoldi <ianaz90@gmail.com>
2021-06-03 14:10:57 +0200

    Added timestamp

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,5 +5,7 @@ export function handleNewEvent(claim: Claim): void {
   let e = new Event(claim.params.hash.toHexString()+"-"+claim.params.file.toHexString())
   e.claimHash = claim.params.hash
   e.fileHash = claim.params.file
+  e.timestamp = claim.block.timestamp
+
   e.save()
 }
